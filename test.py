c567c9a0a5ce153d06dea080ef2b06a4f297c305
Rostislav Strakhov <r.r.strakhov@student.khai.edu>
2021-10-04 09:46:56 +0300

    Resolved conflict

--- test.py
+++ test.py
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
-print(“Python education”)
+print("Python education")
 
-=======
-print("education")
-# new string
->>>>>>> c2a48a9 (test.py added to feature_branch with change on string#2)
