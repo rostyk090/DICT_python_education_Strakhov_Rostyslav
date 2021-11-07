19d8dbb28c5bea3374b0690cf00f7d5259092702
Rostislav Strakhov <r.r.strakhov@student.khai.edu>
2021-11-07 16:31:56 +0200

    Hangman

---
+++ Hangman/hangman.py
@@ -0,0 +1,13 @@
+import random
+print('HANGMAN')
+print('The game will be available soon')
+print("Guess the word:")
+answer = input()
+if answer == "python":
+    print("You survived!")
+else:
+    print("You lost!")
+
+
+
+
