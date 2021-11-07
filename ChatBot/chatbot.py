67793207fcbdc31df2c5bd731cd3fb72d46697a4
Rostislav Strakhov <r.r.strakhov@student.khai.edu>
2021-11-07 15:29:40 +0200

    Hangman 1-st stage

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -1,2 +1,30 @@
 print("Hello! My name is TurboBot.\nI was created in 2021.")
-
+name = input("Please, remind me your name.\n> ")
+print("What a great name you have," + name + "!")
+print("Let me guess your age.")
+remainder3 = input("Enter remainders of dividing your age by 3, 5 and 7.\n> ")
+remainder5 = input("> ")
+remainder7 = input("> ")
+age = ((int(remainder3) * 70 + int(remainder5) * 21 + int(remainder7) * 15) % 105)
+print("Your age is " + str(age) + "; that's a good time to start programming!")
+qwe = int(input("Now I will prove to you that I can count to any number you want.\n> "))
+qwe1 = list(range(qwe + 1))
+for rty in qwe1:
+    print(str(rty) + "!")
+print("Completed, have a nice day!")
+print("""Let's test your programming knowledge.
+   Why do we use methods?
+1. To repeat a statement multiple times.
+2. To decompose a program into several small subroutines.
+3. To determine the execution time of a program.
+4. To interrupt the execution of a program.""")
+x = 1
+while x == 1:
+    answer = int(input("> "))
+    if answer == 2:
+        print("Completed, have a nice day!")
+        break
+    else:
+        print("Please, try again.")
+        continue
+print("Congratulations, have a nice day!")
