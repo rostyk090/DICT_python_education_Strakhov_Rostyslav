79148bf9ed2b60471b424e872a41cb711214ca53
Rostislav Strakhov <r.r.strakhov@student.khai.edu>
2021-11-14 22:29:34 +0200

    Hangman

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,36 +1,4 @@
 import random
-print('HANGMAN')
-print('The game will be available soon')
-print("Guess the word:")
-answer = input()
-if answer == "python":
-    print("You survived!")
-else:
-    print("You lost!")
-import random
-print("HANGMAN")
-print("The game will be available soon.")
-list_words = ['python', 'javascript', 'php', 'java']
-answer_prog = random.choice(list_words)
-print("Guess the word:")
-answer == input()
-if answer == answer_prog:
-    print("You survived!")
-else:
-    print("You lost!")
-import random
-print("HANGMAN")
-print("The game will be available soon.")
-list_words = ['python', 'javascript', 'php', 'java']
-answer_prog = random.choice(list_words)
-help_prog = answer_prog[0] + answer_prog[1] + answer_prog[2] + '-' * (len(answer_prog) -3)
-print("Guess the word " + help_prog + ":")
-answer = input()
-if answer == answer_prog:
-    print("You survived!")
-else:
-    print("You lost!")
-import random
 print("HANGMAN")
 print("The game will be available soon.")
 words = ['python', 'javascript', 'php', 'java']
@@ -38,11 +6,17 @@ answer_prog = random.choice(words)  # python
 word_list = list(answer_prog)
 user_word_list_null = "-" * len(answer_prog)
 user_list = list(user_word_list_null)
+user_used = []
 print(user_word_list_null)
 count = 0
 while count != 8:
     count += 1
     answer_user = str(input('Input a letter:'))
+    if answer_user in word_list:
+        print('No improvements')
+        print(''.join(user_list))
+        continue
+    user_used.append(answer_user)
     if answer_user in word_list:
         if word_list.count(answer_user) >= 2:
             index = word_list.index(answer_user)
@@ -50,10 +24,15 @@ while count != 8:
             word_list[index] = '-'
         index = word_list.index(answer_user)
         user_list[index] = answer_user
-    else:
+        count -= 1
+    elif answer_user not in (user_used and word_list):
         print("That letter does not appear in the word")
     print(''.join(user_list))
-print("Thanks for playing!")
-print("We will see how well you di in the text stage")
+word_list = list(answer_user)
+if user_list != word_list:
+    print("You lost!")
+else:
+    print("You guessed the word!")
+    print("You survived!")
 
 
