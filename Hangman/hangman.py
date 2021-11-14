54bff7a2a3bc0486cb0e7012cd33a89898141beb
Rostislav Strakhov <r.r.strakhov@student.khai.edu>
2021-11-14 18:29:44 +0200

    Hangman

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -18,6 +18,18 @@ if answer == answer_prog:
     print("You survived!")
 else:
     print("You lost!")
+import random
+print("HANGMAN")
+print("The game will be available soon.")
+list_words = ['python', 'javascript', 'php', 'java']
+answer_prog = random.choice(list_words)
+help_prog = answer_prog[0] + answer_prog[1] + answer_prog[2] + '-' * (len(answer_prog) -3)
+print("Guess the word " + help_prog + ":")
+answer = input()
+if answer == answer_prog:
+    print("You survived!")
+else:
+    print("You lost!")
 
 
 
