af455721ee4a40a76ae832a256cfcdf5cd388925
Rostislav Strakhov <r.r.strakhov@student.khai.edu>
2021-11-14 19:04:13 +0200

    Hangman

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -30,6 +30,30 @@ if answer == answer_prog:
     print("You survived!")
 else:
     print("You lost!")
-
+import random
+print("HANGMAN")
+print("The game will be available soon.")
+words = ['python', 'javascript', 'php', 'java']
+answer_prog = random.choice(words)  # python
+word_list = list(answer_prog)
+user_word_list_null = "-" * len(answer_prog)
+user_list = list(user_word_list_null)
+print(user_word_list_null)
+count = 0
+while count != 8:
+    count += 1
+    answer_user = str(input('Input a letter:'))
+    if answer_user in word_list:
+        if word_list.count(answer_user) >= 2:
+            index = word_list.index(answer_user)
+            user_list[index] = answer_user
+            word_list[index] = '-'
+        index = word_list.index(answer_user)
+        user_list[index] = answer_user
+    else:
+        print("That letter does not appear in the word")
+    print(''.join(user_list))
+print("Thanks for playing!")
+print("We will see how well you di in the text stage")
 
 
