a2b71b6373ad8e8e1de3575ca9f0a0447236dc11
Rostislav Strakhov <r.r.strakhov@student.khai.edu>
2021-11-15 08:55:47 +0200

    Hangman

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -8,17 +8,17 @@ letter = ""
 user_word_list_null = "-" * len(answer_prog)
 user_list = list(user_word_list_null)
 user_used = []
+english_letters = list("qwertyuiopasdfghjklzxcvbnm")
 print(user_word_list_null)
 count = 0
 while count != 8:
     count += 1
     answer_user = str(input('Input a letter:'))
-    if answer_user in letter:
-        print('No improvements')
+    if answer_user in user_used:
+        print("You have already guessed this letter")
+        count -= 1
         print(''.join(user_list))
         continue
-    else:
-        letter += answer_user
     user_used.append(answer_user)
     if answer_user in word_list:
         if word_list.count(answer_user) >= 2:
@@ -28,6 +28,14 @@ while count != 8:
         index = word_list.index(answer_user)
         user_list[index] = answer_user
         count -= 1
+    elif answer_user not in english_letters:
+        print("Please enter a lowercase English letter")
+        count -= 1
+        continue
+    elif len(answer_user) >= 2:
+        print("You should input a single letter")
+        count -= 1
+        continue
     elif answer_user not in (user_used and word_list):
         print("That letter does not appear in the word")
     print(''.join(user_list))
@@ -36,6 +44,7 @@ if user_list != word_list:
     print("You lost!")
 else:
     print("You guessed the word!")
-    print("You survived!")
+    print("You survives!")
+
 
 
