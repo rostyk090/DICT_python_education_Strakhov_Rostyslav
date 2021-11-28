724e79cfdf92b5eea82df8a582a2f602555ac842
Rostislav Strakhov <r.r.strakhov@student.khai.edu>
2021-11-28 17:48:19 +0200

    2-th stage

--- CoffeMachine/CoffeMachine.py
+++ CoffeMachine/CoffeMachine.py
@@ -5,3 +5,14 @@ Mixing boiled water with crushed coffee beans
 Pouring coffee into the cup
 Pouring some milk into the cup
 Coffee is ready!""")
+coffee_water = 200
+coffee_milk = 50
+coffee_beans = 15
+count_cups = int(input("\nWrite how many cups of coffee you will need:"))
+water = count_cups * coffee_water
+milk = count_cups * coffee_milk
+beans = count_cups * coffee_beans
+print('For ' + str(count_cups) + ' cups of coffee you will need:')
+print(str(water) + ' ml of water')
+print(str(milk) + ' ml of milk')
+print(str(beans) + ' g of coffee beans')
