06077c426a3d24262ebf3eb74e7d3d9faea07e30
Rostislav Strakhov <r.r.strakhov@student.khai.edu>
2021-12-12 20:26:15 +0200

    4-th stage

--- TicTacToe/TicTacToe.py
+++ TicTacToe/TicTacToe.py
@@ -8,7 +8,7 @@ win = 0
 def play():
     print("---------")
     print("| ", end="")
-    for a in c[1:3]:
+    for a in c[:3]:
         print(a, end=" ")
     print("|")
     print("| ", end="")
@@ -50,3 +50,90 @@ def game_part():
     elif c_ < 1:
         win += 1
         print("Draw")
+
+play()
+tried = "X"
+while True:
+    inp = input("Enter the coordinates: ")
+    cc = list(inp)
+    x, y = str(cc[2]), str(cc[0])
+    if x.isalpha() or y.isalpha() or x == " " or y == " ":
+        print("You should enter numbers!")
+        continue
+    elif y == "1":
+        if x == "1":
+            if c[0] != "X" and c[0] != "O":
+                c[0] = tried
+            else:
+                print("This cell is occupied! Choose another one!")
+                continue
+        elif x == "2":
+            if c[1] != "X" and c[1] != "O":
+                c[1] = tried
+            else:
+                print("This cell is occupied! Choose another one!")
+                continue
+        elif x == "3":
+            if c[2] != "X" and c[2] != "O":
+                c[2] = tried
+            else:
+                print("This cell is occupied! Choose another one!")
+                continue
+        else:
+            print("Coordinates should be from 1 to 3!")
+            continue
+    elif y == "2":
+        if x == "1":
+            if c[3] != "X" and c[3] != "O":
+                c[3] = tried
+            else:
+                print("This cell is occupied! Choose another one!")
+                continue
+        elif x == "2":
+            if c[4] != "X" and c[4] != "O":
+                c[4] = tried
+            else:
+                print("This cell is occupied! Choose another one!")
+                continue
+        elif x == "3":
+            if c[5] != "X" and c[5] != "O":
+                c[5] = tried
+            else:
+                print("This cell is occupied! Choose another one!")
+                continue
+        else:
+            print("Coordinates should be from 1 to 3!")
+            continue
+    elif y == "3":
+        if x == "1":
+            if c[6] != "X" and c[6] != "O":
+                c[6] = tried
+            else:
+                print("This cell is occupied! Choose another one!")
+                continue
+        elif x == "2":
+            if c[7] != "X" and c[7] != "O":
+                c[7] = tried
+            else:
+                print("This cell is occupied! Choose another one!")
+                continue
+        elif x == "3":
+            if c[8] != "X" and c[8] != "O":
+                c[8] = tried
+            else:
+                print("This cell is occupied! Choose another one!")
+                continue
+        else:
+            print("Coordinates should be from 1 to 3!")
+            continue
+    else:
+        print("Coordinates should be from 1 to 3!")
+        continue
+    play()
+    game_part()
+    if win > 0:
+        break
+    if tried == "X":
+        tried = "O"
+    else:
+        tried = "X"
