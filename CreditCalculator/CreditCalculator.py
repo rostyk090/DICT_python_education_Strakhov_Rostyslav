0f9d4c65b0a92aa5eb5f9c3b39c831a6bdd3f4c5
Rostislav Strakhov <r.r.strakhov@student.khai.edu>
2021-12-19 18:19:12 +0200

    3-th stage

--- CreditCalculator/CreditCalculator.py
+++ CreditCalculator/CreditCalculator.py
@@ -3,23 +3,44 @@ Month 1: repaid 250
 Month 2: repaid 250
 Month 3: repaid 500
 The loan has been repaid!""")
-import math
+from math import ceil, log
 print('Enter the loan principal:')
-lp = int(input())
 print("""What do you want to calculate?
-type "m" – for number of monthly payments
-type "p" – for the monthly payment:""")
+type "n" for number of monthly payments,
+type "a" for annuity monthly payment amount,
+type "p" for loan principal:""")
 choice = input()
-if choice == 'm':
+if choice == 'n':
+    print('Enter the loan principal:')
+    p = float(input())
     print('Enter the monthly payment:')
-    mp = int(input())
-    print('It will take ' + str(math.ceil(lp / mp)) + ' months to repay the loan')
-else:
-    print('Enter the number of months:')
-    nm = int(input())
-    pm = math.ceil(lp / nm)
-    LastPay = lp - (nm - 1) * pm
-    if LastPay == pm:
-        print('Your monthly payment = ' + str(pm))
-    else:
-        print('Your monthly payment = ' + str(pm) + ' and the last payment = ' + str(LastPay))
+    a = float(input())
+    print('Enter the loan interest:')
+    i = float(input()) / 1200
+    n = ceil(log(a / (a - (i * p)), (1 + i)))
+    if n // 12 == 0:
+        print('It will take ' + str(n) + ' months to repay this loan!')
+    elif n % 12 == 0:
+        print('It will take ' + str(n // 12) + ' years to repay this loan!')
+    elif n % 12 != 0:
+        print('It will take ' + str(n // 12) + ' years and ' + str(n % 12) + ' months to repay this loan!')
+elif choice == 'a':
+    print('Enter the loan principal:')
+    p = float(input())
+    print('Enter the number of periods:')
+    n = float(input())
+    print('Enter the loan interest:')
+    i = float(input()) / 1200
+    i_n = (1 + i) ** n
+    a = ceil(p * (i * i_n / (i_n - 1)))
+    print('Your monthly payment = ' + str(a) + '!')
+elif choice == 'p':
+    print('Enter the annuity payment:')
+    a = float(input())
+    print('Enter the number of periods:')
+    n = float(input())
+    print('Enter the loan interest:')
+    i = float(input()) / 1200
+    i_n = (1 + i) ** n
+    p = round(a / (i * i_n / (i_n - 1)))
+    print('Your loan principal = ' + str(p) + '!')
